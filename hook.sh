0669b9e10788b0700f25f54942319ae66935b396
Jeffrey 'jf' Lim <jf@users.noreply.github.com>
2019-12-14 15:31:23 +0800

    {hook,clean}.sh: tidy up formatting (thank you for using tabs! ;)); also correct spelling for 'Recieved'

--- hook.sh
+++ hook.sh
@@ -8,7 +8,7 @@ while [ -h "$SOURCE"  ]; do
   [[ $SOURCE != /*  ]] && SOURCE="$DIR/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd  )"
-echo "Recieved request for" "${CERTBOT_DOMAIN}"
+echo "Received request for" "${CERTBOT_DOMAIN}"
 cd ${DIR}
 source config.sh
 
@@ -17,14 +17,17 @@ VALIDATION=${CERTBOT_VALIDATION}
 
 ## Get the XML & record ID
 curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIKEY&domain=$DOMAIN" > $CACHE$DOMAIN.xml
+
 ## Check for existing ACME record
 if grep -q "_acme-challenge" $CACHE$DOMAIN.xml
 then
+
 	## Get record ID
-    RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '_acme-challenge.$DOMAIN' ]" $CACHE$DOMAIN.xml | grep -oP '(?<=<record_id>).*?(?=</record_id>)'`
+	RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '_acme-challenge.$DOMAIN' ]" $CACHE$DOMAIN.xml | grep -oP '(?<=<record_id>).*?(?=</record_id>)'`
 	## Update DNS record in Namesilo:
 	curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=_acme-challenge&rrvalue=$VALIDATION&rrttl=3600" > $RESPONSE
 	RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()"  $RESPONSE`
+
 	## Process response, maybe wait
 	case $RESPONSE_CODE in
 		300)
@@ -35,24 +38,27 @@ then
 				sleep 60s
 			done
 			;;
-	   280)
-		RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
-		echo "Update aborted, please check your NameSilo account."
-		echo "Domain: $DOMAIN"
-		echo "rrid: $RECORD_ID"
-		echo "reason: $RESPONSE_DETAIL"
+		280)
+			RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
+			echo "Update aborted, please check your NameSilo account."
+			echo "Domain: $DOMAIN"
+			echo "rrid: $RECORD_ID"
+			echo "reason: $RESPONSE_DETAIL"
 			;;
-	   *)
-		echo "Namesilo returned code: $RESPONSE_CODE"
-		echo "Reason: $RESPONSE_DETAIL"
-		echo "Domain: $DOMAIN"
-		echo "rrid: $RECORD_ID"
+		*)
+			echo "Namesilo returned code: $RESPONSE_CODE"
+			echo "Reason: $RESPONSE_DETAIL"
+			echo "Domain: $DOMAIN"
+			echo "rrid: $RECORD_ID"
 			;;
 	esac
+
 else
+
 	## Add the record
 	curl -s "https://www.namesilo.com/api/dnsAddRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrtype=TXT&rrhost=_acme-challenge&rrvalue=$VALIDATION&rrttl=3600" > $RESPONSE
 	RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()"  $RESPONSE`
+
 	## Process response, maybe wait
 	case $RESPONSE_CODE in
 		300)
@@ -63,20 +69,20 @@ else
 				sleep 60s
 			done
 			;;
-	   280)
-
-		RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
-		echo "DNS addition aborted, please check your NameSilo account."
-		echo "Domain: $DOMAIN"
-		echo "rrid: $RECORD_ID"
-		echo "reason: $RESPONSE_DETAIL"
+		280)
+			RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
+			echo "DNS addition aborted, please check your NameSilo account."
+			echo "Domain: $DOMAIN"
+			echo "rrid: $RECORD_ID"
+			echo "reason: $RESPONSE_DETAIL"
 			;;
-	   *)
-		RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
-		echo "Namesilo returned code: $RESPONSE_CODE"
-		echo "Reason: $RESPONSE_DETAIL"
-		echo "Domain: $DOMAIN"
-		echo "rrid: $RECORD_ID"
+		*)
+			RESPONSE_DETAIL=`xmllint --xpath "//namesilo/reply/detail/text()"  $RESPONSE`
+			echo "Namesilo returned code: $RESPONSE_CODE"
+			echo "Reason: $RESPONSE_DETAIL"
+			echo "Domain: $DOMAIN"
+			echo "rrid: $RECORD_ID"
 			;;
 	esac
+
 fi
